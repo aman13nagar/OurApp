33c1a6060381d0f8a3d69dfd1d91568c3a693a9b
aman13nagar <122695931+aman13nagar@users.noreply.github.com>
2023-07-28 18:44:53 +0530

    f commit

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ devServer: {
 }
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 app.use(session({
   secret:"Our little secret.",
